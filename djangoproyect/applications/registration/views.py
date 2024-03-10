69934bc73dbd3be1556479e84c5c3d075448be61
JMMA86 <1126597295@u.icesi.edu.co>
2024-03-09 23:45:58 -0500

    Added connection with bd

--- djangoproyect/applications/registration/views.py
+++ djangoproyect/applications/registration/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from applications.login import views
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 
 def register_view(request):
     if request.method == "GET":
@@ -19,6 +20,12 @@ def register_view(request):
                     request,
                     "register.html",
                     {"message": "El usuario ya está registrado.", "user_data": user_data},
+                )
+            if int(request.POST["cedula"]) <= 1000000 or len(request.POST["cedula"]) > 12:
+                return render(
+                    request,
+                    "register.html",
+                    {"message": "La cédula ingresada no es válida.", "user_data": user_data},
                 )
             elif not request.POST["correo"].endswith("@u.icesi.edu.co") and not request.POST["correo"].endswith("@icesi.edu.co"):
                 return render(
@@ -39,9 +46,19 @@ def register_view(request):
                     {"message": "Las contraseñas no coinciden.", "user_data": user_data},
                 )
             else:
+                user = User.objects.create_user(
+                id=request.POST["cedula"],
+                username=request.POST["cedula"],
+                first_name=request.POST["nombre"],
+                last_name=request.POST["apellido"],
+                password=request.POST["contrasena"],
+                email=request.POST["correo"],
+                )
+                user.save()
                 messages.success(request, 'Usuario registrado correctamente.')
                 return redirect('login:login_view')
-        except:
+        except Exception as e:
+            print(e)
             return render(
                 request,
                 "register.html",
