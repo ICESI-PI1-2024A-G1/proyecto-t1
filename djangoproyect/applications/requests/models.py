cd620690bdfe345885ed741e7f0552f9cb28382b
elkofix <holasoygermamwow@gmail.com>
2024-03-10 19:19:42 -0500

    feat(save_traceability): expand data model

--- djangoproyect/applications/requests/models.py
+++ djangoproyect/applications/requests/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Involved(models.Model):
+    email = models.CharField(max_length=320)  
+    name = models.CharField(max_length=50)  
 
 # Create your models here.
 class Requests(models.Model):
@@ -10,3 +13,12 @@ class Requests(models.Model):
     final_date = models.DateField()
     past_days = models.IntegerField()
     status = models.CharField(max_length=20)
+    beneficiary = models.ForeignKey(Involved, on_delete=models.CASCADE, related_name="beneficiario")
+    reviewer = models.ForeignKey(Involved, on_delete=models.CASCADE, related_name="revisor")
+    final_approver = models.ForeignKey(Involved, on_delete=models.CASCADE, related_name="aprobador_final")
+
+
+class Traceability(models.Model):
+    involved = models.ForeignKey(Involved, on_delete=models.CASCADE)
+    request = models.ForeignKey(Requests, on_delete=models.CASCADE)
+    date = models.DateField()
