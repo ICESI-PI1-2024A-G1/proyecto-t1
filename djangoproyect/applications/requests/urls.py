eac16af823deee5c50dd199c15c3a403dea2e2d1
JMMA86 <1126597295@u.icesi.edu.co>
2024-03-24 20:41:14 -0500

    chore: Added tests for secure_routes (Admin)

--- djangoproyect/applications/requests/urls.py
+++ djangoproyect/applications/requests/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from applications.requests import views
 
-
 urlpatterns = [
     path("change-request/<int:id>", views.change_requests),
     path("search/<str:query>", views.search, name="search"),
--- djangoproyect/applications/requests/views.py
+++ djangoproyect/applications/requests/views.py
@@ -6,7 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
 @csrf_exempt
-@login_required
 def change_requests(request, id):
     if request.method == "POST":
         # Get the request by its ID
