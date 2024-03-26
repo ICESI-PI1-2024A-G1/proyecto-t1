4d67fca3c42f391933628db681fed8158f4a99dd
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-03-26 11:04:19 -0500

    refactor: change teams view to be a table, and avoid loading all members of all teams

--- djangoproyect/applications/teams/views.py
+++ djangoproyect/applications/teams/views.py
@@ -61,59 +61,6 @@ def delete_team(request, team_id):
 ### MEMBERS VIEWS
 
 
-def delete_member(request, team_id, member_id):
-    team = Team.objects.get(id=team_id)
-    team.members.remove(member_id)
-    team.save()
-    return JsonResponse(
-        {"message": f"El miembro {member_id} ha sido eliminado correctamente"}
-    )
-
-
-def add_member(request, team_id):
-    if request.method == "GET":
-        team = get_object_or_404(Team, id=team_id)
-        users = User.objects.exclude(id__in=team.members.all()).exclude(
-            id=team.leader.id
-        )
-        users_data = [
-            {
-                "id": user.id,
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-                "username": user.username,
-            }
-            for user in users
-        ]
-
-        return JsonResponse(users_data, safe=False)
-    elif request.method == "POST":
-        team = get_object_or_404(Team, id=team_id)
-        selected_users_ids = [
-            int(user_id) for user_id in request.POST.getlist("users[]")
-        ]
-        team.members.add(*selected_users_ids)
-        team.save()
-
-        new_members = User.objects.filter(id__in=selected_users_ids)
-        new_members_data = [
-            {
-                "id": user.id,
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-                "username": user.username,
-            }
-            for user in new_members
-        ]
-
-        return JsonResponse(
-            {
-                "message": "Miembros añadidos con éxito al equipo.",
-                "users": new_members_data,
-            }
-        )
-
-
 def assign_requests(request, id):
     if request.method == "GET":
         member = get_object_or_404(User, pk=id)
