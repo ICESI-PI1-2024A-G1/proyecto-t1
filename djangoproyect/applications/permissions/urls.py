c5d735d395dffdc4f242c5a8e2f9820aa81791c2
JMMA86 <1126597295@u.icesi.edu.co>
2024-03-26 14:25:36 -0500

    feat: Added permissions saver with log message

--- djangoproyect/applications/permissions/urls.py
+++ djangoproyect/applications/permissions/urls.py
@@ -6,4 +6,5 @@ app_name = 'permissions'
 urlpatterns = [
     path("", views.permissions_view, name='permissions_view'),
     path("search/<str:query>", views.search, name="search"),
+    path('update_user_permissions/', views.update_user_permissions, name='update_user_permissions'),
 ]
--- djangoproyect/applications/permissions/views.py
+++ djangoproyect/applications/permissions/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.db.models import Case, When, Value, IntegerField
 from applications.permissions.model.filter_logic import SearchFilter
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib import messages
+import json
 
 
 User = get_user_model()
@@ -24,3 +28,19 @@ def search(request, query):
     users_filter = SearchFilter()
 
     return users_filter.filter_users(query)
+
+
+@csrf_exempt
+def update_user_permissions(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        for item in data:
+            user = User.objects.get(id=item['id'])
+            if 'is_staff' in item:
+                user.is_staff = item['is_staff']
+            if 'is_leader' in item:
+                user.is_leader = item['is_leader']
+            user.save()
+        return JsonResponse({'status': 'success', 'message': 'Permisos actualizados correctamente.'})
+    else:
+        return JsonResponse({'status': 'failed', 'message': 'Los permisos no se pudieron actualizar.'})
