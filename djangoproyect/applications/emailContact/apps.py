a6cfc38540ba573db9ce1486ec6c18e34edd8872
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-03-30 18:44:54 -0500

    doc: Fully documented email_contact app

--- djangoproyect/applications/emailContact/apps.py
+++ djangoproyect/applications/emailContact/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class EmailcontactConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'applications.emailContact'
+    """App configuration for emailContact."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "applications.emailContact"
