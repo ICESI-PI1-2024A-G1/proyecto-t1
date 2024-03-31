2fd10834bde8086debfba6c8a36f90dc19dfcfda
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-03-31 13:56:21 -0500

    docs: add documentation for teams static js functions

--- djangoproyect/static/teams/js/teams.js
+++ djangoproyect/static/teams/js/teams.js
@@ -1,24 +1,36 @@
-
+/**
+ * Initializes the DataTable for the specified table element.
+ * @param {string} tableId - The ID of the table element to initialize as a DataTable.
+ */
 $(document).ready(function () {
-    DataTableInit("teamsTable")
+    DataTableInit("teamsTable");
 
+    // Fetch CSRF token from meta tag
     const csrfTokenMeta = document.querySelector('meta[name="csrf_token"]');
     const csrftoken = csrfTokenMeta.getAttribute('content');
+
+    /**
+     * Event handler for clicking on the delete team button.
+     */
     $(".delete-team").click(function() {
         var teamId = $(this).data("team-id");
-        if (confirm("¿Estás seguro de que quieres eliminar este equipo?")) {
+        if (confirm("Are you sure you want to delete this team?")) {
+            // Send AJAX request to delete team
             $.ajax({
                 url: `/teams/delete/${teamId}/`,
                 type: "DELETE",
                 beforeSend: function(xhr) {
+                    // Set CSRF token in request header
                     xhr.setRequestHeader("X-CSRFToken", csrftoken);
                 },
                 success: function(response) {
-                    location.reload()
+                    // Reload the page after successful deletion
+                    location.reload();
                 },
                 error: function(xhr, status, error) {
                     console.error(error);
-                    alert("Error al eliminar el equipo");
+                    // Display an alert for error in deletion
+                    alert("Error deleting team");
                 }
             });
         }
