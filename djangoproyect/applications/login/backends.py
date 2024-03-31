6345debc136ff1ece95a05f3c6ea8e24d861de3f
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-03-31 15:01:42 -0500

    docs: add documentation for the configuration files for login

--- djangoproyect/applications/login/backends.py
+++ djangoproyect/applications/login/backends.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
+
 class IDBackend(ModelBackend):
+    """
+    Custom authentication backend for authenticating users by ID.
+    """
+
     def authenticate(self, request, id=None, password=None, **kwargs):
+        """Authenticate a user by ID and password.
+
+        Args:
+            request: HttpRequest object.
+            id (str): User ID to authenticate.
+            password (str): User password to authenticate.
+
+        Returns:
+            User object: Authenticated user if successful; else, returns None.
+        """
         UserModel = get_user_model()
         try:
             user = UserModel.objects.get(id=id)
@@ -12,6 +27,14 @@ class IDBackend(ModelBackend):
             return None
 
     def get_user(self, user_id):
+        """Retrieve a user by ID.
+
+        Args:
+            user_id (str): User ID to retrieve.
+
+        Returns:
+            User object: User if found; else, returns None.
+        """
         UserModel = get_user_model()
         try:
             return UserModel.objects.get(pk=user_id)
