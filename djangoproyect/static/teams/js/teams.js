085e91be74c43dc22673e57416724f81b9069f48
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-03-31 14:46:42 -0500

    doc: add documentation for teams js static file

--- djangoproyect/static/teams/js/teams.js
+++ djangoproyect/static/teams/js/teams.js
@@ -1,11 +1,23 @@
-
 $(document).ready(function () {
+    /**
+     * Initializes DataTable with the given table ID.
+     *
+     * @param {string} tableId - The ID of the table to initialize DataTable on.
+     */
     DataTableInit("teamsTable")
 
     const csrfTokenMeta = document.querySelector('meta[name="csrf_token"]');
     const csrftoken = csrfTokenMeta.getAttribute('content');
+
+    /**
+     * Handles click event on delete team button.
+     */
     $(".delete-team").click(function() {
         var teamId = $(this).data("team-id");
+
+        /**
+         * Confirms and deletes the team if confirmed.
+         */
         if (confirm("¿Estás seguro de que quieres eliminar este equipo?")) {
             $.ajax({
                 url: `/teams/delete/${teamId}/`,
