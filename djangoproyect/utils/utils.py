7e5930e2bc471a74b475b2b27c7fd31fc153353f
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-03-31 14:28:36 -0500

    doc: add documentation for email sender utils

--- djangoproyect/utils/utils.py
+++ djangoproyect/utils/utils.py
@@ -6,6 +6,19 @@ import string
 
 
 def send_verification_email(request, subject, bigSubject, email, message):
+    """
+    Sends a verification email using Django's EmailMessage.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+        subject (str): The subject of the email.
+        bigSubject (str): The "from" field of the email.
+        email (str): The recipient's email address.
+        message (str): The message body of the email.
+
+    Returns:
+        None
+    """
     template = render_to_string(
         "email_template.html",
         {
@@ -29,5 +42,14 @@ def send_verification_email(request, subject, bigSubject, email, message):
 
 
 def generate_random_code(length=6):
+    """
+    Generates a random alphanumeric code of a specified length.
+
+    Parameters:
+        length (int): The length of the generated code. Default is 6.
+
+    Returns:
+        str: A random alphanumeric code.
+    """
     characters = string.ascii_uppercase + string.digits
     return "".join(random.choice(characters) for _ in range(length))
