7e6a9c96882be20f2fc2b358fe50f64e59904b83
Juan Manuel Marin Angarita <1126597295@u.icesi.edu.co>
2024-05-04 16:46:38 -0500

    feat: Added change final date option

--- src/apps/internalRequests/urls.py
+++ src/apps/internalRequests/urls.py
@@ -12,6 +12,7 @@ app_name = "internalRequests"
 urlpatterns = [
     path("", views.show_requests, name="show_requests"),
     path("change-status/<int:id>", views.change_status, name="change_status"),
+    path("change-final-date/<int:id>", views.change_final_date, name="change_final_date"),
     path("assign-request/<int:request_id>", views.assign_request, name="assign_request"),
     path("<int:id>/", views.detail_request, name="request_detail"),
     path("show-traceability/<int:request_id>", views.show_traceability, name="show_traceability"),
