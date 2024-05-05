52ea3457a6b48128cac8e53a3f7aa7f8c32b7170
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-05-04 20:54:15 -0500

    chore: creating a build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+npm install
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+python generate.py shell
+
+# Apply any outstanding database migrations
+python manage.py migrate
