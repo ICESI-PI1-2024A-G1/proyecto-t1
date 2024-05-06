26efadd77923c469bdf142401150e1e13fe07344
OscarGomez-jpg <os.gomez.lozano@gmail.com>
2024-05-06 00:01:28 -0500

    chore: changed redirect from /sharepoint to /sharepoint/

--- apps/login/views.py
+++ apps/login/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 import utils.utils as utils
 import subprocess
 import os
+
 User = get_user_model()
 
 
@@ -31,7 +32,7 @@ def login_view(request):
         request.session["has_requested_password"] = False
         if request.user.is_authenticated and request.GET.get("logout") != "true":
             if request.user.is_superuser or request.user.is_leader:
-                return redirect("/sharepoint")
+                return redirect("/sharepoint/")
             else:
                 return redirect("/requests")
         else:
@@ -51,7 +52,9 @@ def login_view(request):
                     return render(
                         request,
                         "login.html",
-                        {"message": "Usuario no autorizado. Comuníquese con el administrador."},
+                        {
+                            "message": "Usuario no autorizado. Comuníquese con el administrador."
+                        },
                     )
                 request.session["user_id"] = user.id
 
@@ -61,7 +64,7 @@ def login_view(request):
                 ruta_script = "apps/login/write.py"
                 comando = f"python {ruta_script} {random_code}"
                 subprocess.call(comando, shell=True)
-                
+
                 print("Code: " + random_code)
 
                 # Send verification email
@@ -114,7 +117,7 @@ def verify_email_view(request):
             if request.user.is_authenticated:
                 if request.user.is_superuser or request.user.is_leader:
                     if request.user.is_superuser or request.user.is_leader:
-                        return redirect("/sharepoint")
+                        return redirect("/sharepoint/")
                     else:
                         return redirect("/requests")
                 else:
@@ -129,7 +132,7 @@ def verify_email_view(request):
             user.backend = backend
             login(request, user)
             if request.user.is_superuser or request.user.is_leader:
-                return redirect("/sharepoint")
+                return redirect("/sharepoint/")
             else:
                 return redirect("/requests")
         else:
@@ -172,7 +175,7 @@ def reset_password_view(request):
             ruta_script = "apps/login/write.py"
             comando = f"python {ruta_script} {random_code}"
             subprocess.call(comando, shell=True)
-                
+
             request.session["random_code"] = random_code
             # print("Code: " + random_code)
             user = User.objects.get(id=id)
@@ -215,7 +218,7 @@ def verify_email_reset_view(request):
         else:
             if request.user.is_authenticated:
                 if request.user.is_superuser or request.user.is_leader:
-                    return redirect("/sharepoint")
+                    return redirect("/sharepoint/")
                 else:
                     return redirect("/requests")
             else:
@@ -248,7 +251,7 @@ def change_password_view(request):
         else:
             if request.user.is_authenticated:
                 if request.user.is_superuser or request.user.is_leader:
-                    return redirect("/sharepoint")
+                    return redirect("/sharepoint/")
                 else:
                     return redirect("/requests")
             else:
