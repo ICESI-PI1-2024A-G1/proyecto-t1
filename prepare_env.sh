dbf9fd9a2b36b60aa9fc3b7cd6e60ed5eef4ad74
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-05-10 11:19:04 -0500

    fix: permission tests name, sharepoint detail url

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,9 +1,8 @@
 pip install -r requirements.txt
-npm install
 cat .env > tempenv.txt
 cat env_test.txt > .env
-rm db.sqlite3
 python3 manage.py makemigrations
+rm db.sqlite3
 python3 manage.py migrate
 python3 generate_for_test.py shell
-python3 manage.py runserver &    
+python3 manage.py runserver &   
--- run_test.sh
+++ run_test.sh
@@ -7,10 +7,10 @@ coverage run manage.py test apps.registration.tests.func_test_registration
 coverage run manage.py test apps.requests.tests.tests_func_request
 coverage run manage.py test apps.internalRequests.tests
 coverage run manage.py test apps.forms.tests
-coverage run manage.py test apps.permissions.tests
 coverage run manage.py test apps.teams.tests.test_func_teams
 coverage run manage.py test apps.teams.tests.test_special
 python3 generate_for_test.py
+coverage run manage.py test apps.permissions.tests
 python3 generate.py shell
 coverage report
 python3 print_results.py
