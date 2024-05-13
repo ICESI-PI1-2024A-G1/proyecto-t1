3bab654a7499be352b9ebc9155bf4dfa8b3b0fd8
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-05-13 18:41:14 -0500

    test: add testing to achieve coverage over 90

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,13 +1,6 @@
 DESTRUCTIVE=false
 
 coverage erase
-coverage run -a manage.py test apps.requests.tests.tests_sharepoint_api
-coverage run -a manage.py test utils.testAdmin
-coverage run -a manage.py test utils.testModel
-coverage run -a manage.py test utils.test
-coverage run -a manage.py test apps.permissions.test_unit
-coverage run -a manage.py test apps.emailContact.tests
-coverage run -a manage.py test apps.login.tests.tests_login
 while (( "$#" )); do
   case "$1" in
     --destructive)
@@ -24,12 +17,21 @@ done
 if [ "$DESTRUCTIVE" = true ]; then
     coverage run manage.py test apps.login.tests.test_backends
 fi
+coverage run -a manage.py test apps.requests.tests.tests_sharepoint_api 
+coverage run -a manage.py test utils.testAdmin
+coverage run -a manage.py test utils.testModel
+coverage run -a manage.py test utils.test
+coverage run -a manage.py test apps.permissions.test_unit
+coverage run -a manage.py test apps.emailContact.tests
+coverage run -a manage.py test apps.login.tests.tests_login
 coverage run -a manage.py test apps.registration.tests.tests_registration
 coverage run -a manage.py test apps.requests.tests.tests_requests
 coverage run -a manage.py test apps.errorHandler.tests
 coverage run -a manage.py test apps.teams.tests.tests_teams
 coverage run -a manage.py test apps.forms.unit_tests
+coverage run -a manage.py test apps.notifications.tests
 coverage run -a manage.py test apps.internalRequests.test_unit
+coverage run -a manage.py test apps.permissions.unit_tests
 
 coverage report
 coverage html
