8dd82dc8ac2133cfba586be804cf0b03b1f81016
ChrisBotina7823 <1080691521@u.icesi.edu.co>
2024-05-13 23:45:58 -0500

    fix: email contact tests and routes

--- run_selenium_tests.sh
+++ run_selenium_tests.sh
@@ -1,9 +1,9 @@
 python3 generate_for_test.py
-coverage run manage.py test apps.login.tests.func_login_test
-coverage run manage.py test apps.registration.tests.func_test_registration
-coverage run manage.py test apps.requests.tests.tests_func_request
-coverage run manage.py test apps.internalRequests.tests
-coverage run manage.py test apps.forms.tests
-coverage run manage.py test apps.teams.tests.test_func_teams
-coverage run manage.py test apps.teams.tests.test_special
-coverage run manage.py test apps.permissions.tests
+coverage run manage.py test apps.login.tests.func_login_test --timing
+coverage run manage.py test apps.registration.tests.func_test_registration --timing
+coverage run manage.py test apps.requests.tests.tests_func_request --timing
+coverage run manage.py test apps.internalRequests.tests --timing
+coverage run manage.py test apps.forms.tests --timing
+coverage run manage.py test apps.teams.tests.test_func_teams --timing
+coverage run manage.py test apps.teams.tests.test_special --timing
+coverage run manage.py test apps.permissions.tests --timing
--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -17,21 +17,21 @@ done
 if [ "$DESTRUCTIVE" = true ]; then
     coverage run manage.py test apps.login.tests.test_backends
 fi
-coverage run -a manage.py test apps.requests.tests.tests_sharepoint_api 
-coverage run -a manage.py test utils.testAdmin
-coverage run -a manage.py test utils.testModel
-coverage run -a manage.py test utils.test
-coverage run -a manage.py test apps.permissions.test_unit
-coverage run -a manage.py test apps.emailContact.tests
-coverage run -a manage.py test apps.login.tests.tests_login
-coverage run -a manage.py test apps.registration.tests.tests_registration
-coverage run -a manage.py test apps.requests.tests.tests_requests
-coverage run -a manage.py test apps.errorHandler.tests
-coverage run -a manage.py test apps.teams.tests.tests_teams
-coverage run -a manage.py test apps.forms.unit_tests
-coverage run -a manage.py test apps.notifications.tests
-coverage run -a manage.py test apps.internalRequests.test_unit
-coverage run -a manage.py test apps.permissions.unit_tests
+coverage run -a manage.py test apps.requests.tests.tests_sharepoint_api  --timing
+coverage run -a manage.py test utils.testAdmin --timing
+coverage run -a manage.py test utils.testModel --timing
+coverage run -a manage.py test utils.test --timing
+coverage run -a manage.py test apps.permissions.test_unit --timing
+coverage run -a manage.py test apps.emailContact.tests --timing
+coverage run -a manage.py test apps.login.tests.tests_login --timing
+coverage run -a manage.py test apps.registration.tests.tests_registration --timing
+coverage run -a manage.py test apps.requests.tests.tests_requests --timing
+coverage run -a manage.py test apps.errorHandler.tests --timing
+coverage run -a manage.py test apps.teams.tests.tests_teams --timing
+coverage run -a manage.py test apps.forms.unit_tests --timing
+coverage run -a manage.py test apps.notifications.tests --timing
+coverage run -a manage.py test apps.internalRequests.test_unit --timing
+coverage run -a manage.py test apps.permissions.unit_tests --timing
 
 coverage report
 coverage html
